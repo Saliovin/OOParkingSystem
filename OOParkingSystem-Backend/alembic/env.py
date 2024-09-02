3678e7f980f6759be0726e13650eaed452ec081a
Saliovin <jonas.eukan@gmail.com>
2024-09-02 20:15:42 +0800

    Added Dockerfile configurations

--- OOParkingSystem-Backend/alembic/env.py
+++ OOParkingSystem-Backend/alembic/env.py
@@ -1,11 +1,12 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
-from app.models import Base
+from app.database.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,6 +16,8 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", os.getenv("SQLALCHEMY_DATABASE_URL"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
