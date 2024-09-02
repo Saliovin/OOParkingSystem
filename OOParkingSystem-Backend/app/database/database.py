74816b179e33166b8c793df5a3aac10a9ad59019
Saliovin <jonas.eukan@gmail.com>
2024-09-02 21:51:29 +0800

    Removed sqlite argument

--- OOParkingSystem-Backend/app/database/database.py
+++ OOParkingSystem-Backend/app/database/database.py
@@ -6,14 +6,10 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-Base.metadata.create_all(bind=engine)
 
 
 def get_db():
